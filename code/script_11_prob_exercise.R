50f6b74e7ea6a88ae5a8255a5abbcbce14672d79
AkiraTerui <hanabi0111@gmail.com>
2024-09-26 10:11:00 -0400

    add: readme

---
+++ code/script_11_prob_exercise.R
@@ -0,0 +1,16 @@
+# setup -------------------------------------------------------------------
+
+rm(list = ls())
+source(here::here("code/set_library.R"))
+
+
+# exercise 1 --------------------------------------------------------------
+# 1. Generate a variable with 50 observations.
+
+
+# 2. Create a figure similar to Figure 3.3
+
+# exercise 2 --------------------------------------------------------------
+# 1. Generate a variable with 1000 observations.
+
+# 2. Create a figure similar to Figure 3.7
